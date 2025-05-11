ee12fccedc498ff0b78ebd2e34c194875ebae761
PatitoDev <115660028+PatitoDev@users.noreply.github.com>
2025-05-12 00:55:16 +0100

    impemented catpuccin colors

--- Components/CodeEditor/CodeEditor.cs
+++ Components/CodeEditor/CodeEditor.cs
@@ -1,41 +1,28 @@
 using Godot;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using TextMateSharp.Grammars;
-using TextMateSharp.Registry;
-using TextMateSharp.Themes;
 
-public record Scope
-{
-    public required string Name { get; init; }
-    public required string FgColor { get; init; }
-    public required string BgColor { get; init; }
-}
-
-public record CodeToken
-{
-    public required string Content { get; init; }
-    public required IReadOnlyList<Scope> Scopes { get; init; }
-}
 
 public partial class CodeEditor : Control
 {
-    private TextEdit _textEditNode;
-    private string _fileContent = "";
-    private ScrollContainer _textContentNode;
+    private Tokenizer _tokenizer = new();
 
-    private Godot.Theme _theme;
+    private string _fileContent = "";
+
+    private TextEdit? _textEditNode;
+    private ScrollContainer? _textContentNode;
+    private Godot.Theme? _godotEditorTheme;
 
     public override void _Ready()
     {
         _textEditNode = GetNode<TextEdit>("%EditorTextEdit");
         _textContentNode = GetNode<ScrollContainer>("%TextContent");
-        _theme = GD.Load<Godot.Theme>("res://Theme/MainTheme.tres");
+        _godotEditorTheme = GD.Load<Godot.Theme>("res://Theme/CodeEditorTheme.tres");
     }
 
     public void OpenFile(string filePath)
     {
+        if (_textEditNode == null) return;
+
         using var file = Godot.FileAccess.Open(
           filePath,
           Godot.FileAccess.ModeFlags.Read
@@ -44,18 +31,67 @@ public partial class CodeEditor : Control
         _textEditNode.Text = content;
         _fileContent = content;
 
-        var tokens = Tokenize(filePath);
-        GD.Print(tokens);
-        Render(tokens);
+        var tokens = _tokenizer.Tokenize(filePath, content);
+        if (tokens != null) {
+            Render(tokens);
+            return;
+        }
+
+        RenderFlat(content);
     }
 
-    public void Render(IReadOnlyList<IReadOnlyList<CodeToken>> tokens)
+    public void ClearEditor()
     {
+        if (_textContentNode == null) return;
+
         var children = _textContentNode.GetChildren();
         foreach (var child in children)
         {
             _textContentNode.RemoveChild(child);
         }
+    }
+
+    public void RenderFlat(string content)
+    {
+        if (_textContentNode == null) return;
+        ClearEditor();
+
+        var lines = content.Split("/n");
+
+        var documentContainer = new VBoxContainer();
+        var lineIndex = 0;
+
+        foreach (var line in lines)
+        {
+            var lineContainer = new HBoxContainer();
+            lineContainer.Theme = new Godot.Theme(); // todo - use existing theme
+            lineContainer.AddThemeConstantOverride("separation", 0);
+
+            var lineNumberNode = new Label();
+            lineNumberNode.Text = lineIndex.ToString() + "   ";
+            lineNumberNode.Theme = _godotEditorTheme;
+            lineContainer.AddChild(lineNumberNode);
+
+            var lineContent = new Label();
+            lineContent.Text = line;
+            lineContent.Theme = _godotEditorTheme;
+            lineContainer.AddChild(lineContent);
+
+            documentContainer.AddChild(lineContainer);
+
+            lineIndex += 1;
+        }
+        _textContentNode.AddChild(documentContainer);
+    }
+
+    public void Render(IReadOnlyList<IReadOnlyList<CodeToken>> tokens)
+    {
+        if (
+            _textContentNode == null ||
+            _godotEditorTheme == null
+        ) return;
+
+        ClearEditor();
 
         var lines = new VBoxContainer();
         var lineIndex = 0;
@@ -67,29 +103,26 @@ public partial class CodeEditor : Control
 
             var lineNumberNode = new Label();
             lineNumberNode.Text = lineIndex.ToString() + "   ";
-            lineNumberNode.Theme = _theme;
+            lineNumberNode.Theme = _godotEditorTheme;
             line.AddChild(lineNumberNode);
 
             foreach (var token in lineTokens)
             {
                 var label = new Label();
                 label.Text = token.Content;
-                label.Theme = _theme;
+                label.Theme = _godotEditorTheme;
 
-                label.LabelSettings = new LabelSettings();
-
-                foreach (var scope in token.Scopes)
-                {
+                // TODO _ CHECKAL CJLAKJ SLDJLAS JL
+                var scope = token.Scopes[0];
                     if (!string.IsNullOrEmpty(scope.FgColor))
                     {
-                        label.LabelSettings.FontColor = Color.FromString(scope.FgColor, Colors.White);
+                        label.AddThemeColorOverride("font_color", Color.FromString(scope.FgColor, Colors.Red));
                     }
                     if (!string.IsNullOrEmpty(scope.BgColor))
                     {
-                        label.LabelSettings.OutlineColor = Color.FromString(scope.BgColor, Colors.White);
-                        label.LabelSettings.OutlineSize = 5;
+                        label.AddThemeColorOverride("font_outline_color", Color.FromString(scope.BgColor, Colors.White));
+                        label.AddThemeConstantOverride("outline_size", 10);
                     }
-                }
 
                 line.AddChild(label);
             }
@@ -100,54 +133,4 @@ public partial class CodeEditor : Control
 
         _textContentNode.AddChild(lines);
     }
-
-    public IReadOnlyList<IReadOnlyList<CodeToken>> Tokenize(string filePath)
-    {
-        var options = new RegistryOptions(ThemeName.HighContrastDark);
-        var registry = new Registry(options);
-        var grammar = registry.LoadGrammar(options.GetScopeByExtension(Path.GetExtension(filePath)));
-        GD.Print(grammar);
-        var theme = registry.GetTheme();
-
-        IStateStack? ruleStack = null;
-
-        var fileLines = _fileContent.Split("\n");
-
-        var parsedTokens = new List<List<CodeToken>>() { };
-
-        foreach (var line in fileLines)
-        {
-
-            var lineTokens = new List<CodeToken>() { };
-
-            var tokenizeResult = grammar.TokenizeLine(line, ruleStack, System.TimeSpan.MaxValue);
-            ruleStack = tokenizeResult.RuleStack;
-
-            foreach (var token in tokenizeResult.Tokens)
-            {
-                var content = line.Substr(token.StartIndex, token.Length);
-                var themeRules = theme.Match(token.Scopes);
-
-                var codeToken = new CodeToken()
-                {
-                    Content = content,
-                    Scopes = themeRules.Select(rule => (
-                      new Scope()
-                      {
-                          BgColor = theme.GetColor(rule.background),
-                          FgColor = theme.GetColor(rule.foreground),
-                          Name = rule.name
-                      }
-                    ))
-                  .ToList()
-                };
-
-                lineTokens.Add(codeToken);
-            }
-            parsedTokens.Add(lineTokens);
-        }
-
-        return parsedTokens;
-    }
-
 }
