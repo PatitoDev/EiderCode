87aa3cf7904c673df6944fec5e91a5082fd5044a
PatitoDev <115660028+PatitoDev@users.noreply.github.com>
2025-05-18 14:46:30 +0100

    add 'e' motion

--- EiderCode.Engine/MotionBuilder.cs
+++ EiderCode.Engine/MotionBuilder.cs
@@ -62,6 +62,7 @@ public static class MotionBuilder
     { (long)Convert.ToInt32('k') , HandleK },
     { (long)Convert.ToInt32('h') , HandleH },
     { (long)Convert.ToInt32('l') , HandleL },
+    { (long)Convert.ToInt32('e') , HandleE },
   };
 
   public static Motion? HandleMotion(
@@ -288,6 +289,68 @@ public static class MotionBuilder
     };
   }
 
+  // e
+  // Go to the end of the next word
+  // word - characters, numberss or underscore sperated from whitespace
+  private static Motion? HandleE(
+    InputKey key,
+    List<string> lines,
+    EditorPosition cursorPosition,
+    string lastKey = ""
+  )
+  {
+    var charPosition = cursorPosition.CharNumber;
+    var lineNumber = cursorPosition.LineNumber;
+    var currentLine = lines[lineNumber]!;
+
+    var stringToMatch = currentLine.Substring(charPosition);
+    var matches = Regex.Matches(stringToMatch, "(\\w+)|(\\S)");
+
+    if (matches.Count == 0) return null; // search on next line
+
+    var firstMatch = matches[0];
+
+    var endIndex = firstMatch.Index + (firstMatch.Length - 1);
+    var matchedLine = cursorPosition.LineNumber;
+    var match = firstMatch;
+
+    if (endIndex == 0) {
+      // if we are at the end of the word already go the the next word
+      // return as we can't find a secondary match
+      if (matches.Count < 2 && (lines.Count - 1) > lineNumber) {
+        // if we don't have anything to match
+        // try to match with line below
+        var nextLineContent = lines[lineNumber + 1]!;
+        var nextLineMatch = Regex.Match(nextLineContent, "(\\w+)|(\\S)");
+        if (nextLineMatch == null) return null; // nothing found
+        match = nextLineMatch;
+        matchedLine += 1;
+      } else {
+        // get next match
+        match = matches[1];
+      }
+    }
+
+    endIndex = match.Index + (match.Length - 1);
+    if (cursorPosition.LineNumber == matchedLine)
+    {
+      // make sure to add substring offset
+      endIndex += charPosition;
+    }
+
+    return new Motion(){
+      Start = new() {
+        CharNumber = cursorPosition.CharNumber,
+        LineNumber = cursorPosition.LineNumber
+      },
+      End = new(){
+        CharNumber = endIndex,
+        LineNumber = matchedLine
+      },
+      MotionStack = key.KeyCode.ToString()
+    };
+  }
+
   // b
   // Go to the previous word
   // word - characters, numberss or underscore sperated from whitespace
@@ -334,6 +397,5 @@ public static class MotionBuilder
       MotionStack = key.KeyCode.ToString()
     };
   }
-
 }
 
