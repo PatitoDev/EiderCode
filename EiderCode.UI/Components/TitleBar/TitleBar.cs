7269dfcf9cb3df0e00cb2220c737757e0f688b9b
PatitoDev <115660028+PatitoDev@users.noreply.github.com>
2025-05-19 20:35:31 +0100

    titlebar btns

--- EiderCode.UI/Components/TitleBar/TitleBar.cs
+++ EiderCode.UI/Components/TitleBar/TitleBar.cs
@@ -3,29 +3,49 @@ using System;
 
 public partial class TitleBar : PanelContainer
 {
-  private bool _isPressed = false;
-  private Vector2I _pressedOffset = Vector2I.Zero;
+    public override void _Ready()
+    {
+        GetNode<Button>("%MinimizeBtn").Pressed += OnMinimizeClicked;
+        GetNode<Button>("%MaximizeBtn").Pressed += OnMaximizeeClicked;
+        GetNode<Button>("%CloseBtn").Pressed += OnCloseClicked;
+    }
+
+    public void OnMinimizeClicked()
+    {
+      GetWindow().Mode = Window.ModeEnum.Minimized;
+    }
+
+    public void OnMaximizeeClicked()
+    {
+      var window = GetWindow();
+      GD.Print(window.Mode);
+      if (window.Mode == Window.ModeEnum.Maximized)
+      {
+        window.Mode = Window.ModeEnum.Windowed;
+      }
+
+      if (window.Mode == Window.ModeEnum.Windowed)
+      {
+        window.Mode = Window.ModeEnum.Maximized;
+      }
+    }
+
+    public void OnCloseClicked()
+    {
+      GetTree().Quit();
+    }
 
     public override void _GuiInput(InputEvent @event)
     {
         base._GuiInput(@event);
-        if (@event is InputEventMouseButton) {
-          var mouseEv = (InputEventMouseButton) @event;
-          if (mouseEv.ButtonIndex == MouseButton.Left)
-          {
-            _isPressed = mouseEv.IsPressed();
-            _pressedOffset = (Vector2I)mouseEv.GlobalPosition;
-          }
-        }
 
-        if (
-          _isPressed &&
-          @event is InputEventMouseMotion
-        ) {
-          var mouseEv = (InputEventMouseMotion) @event;
-          var mousePos = DisplayServer.MouseGetPosition();
-          GetWindow().Position = mousePos - _pressedOffset;
-          //DisplayServer.WindowSetPosition(mousePos - _pressedOffset);
+        if (@event is InputEventMouseButton)
+        {
+            var mouseEv = (InputEventMouseButton)@event;
+            if (mouseEv.ButtonIndex == MouseButton.Left && mouseEv.Pressed)
+            {
+                GetWindow().StartDrag();
+            }
         }
     }
 }
